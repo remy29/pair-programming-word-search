d77a7412239a996af9c9bd680ed1e0e830315583
remy29 <arthur.d.remy@gmail.com>
2020-11-05 20:07:07 +0000

    fixed bug where vertical word search would only work if the word started at the top row

--- wordsearch.js
+++ wordsearch.js
@@ -22,17 +22,5 @@ const wordSearch = (letters, word) => {
   }
 };
 
-wordSearch([
-  ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-  ['E', 'I', 'I', 'N', 'F', 'E', 'L', 'D'],
-  ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-  ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-  ['F', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-  ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-  ['L', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-  ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-], 'SEINFELD');
-
 
 module.exports = wordSearch;
